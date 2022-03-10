680a7b65b591b58d9af1464f072989f737418f07
Vittor-Emanoel <55211291+Vittor-Emanoel@users.noreply.github.com>
2022-03-09 22:50:49 -0300

    Add files via upload
    Esse contador é um exercícios para quem está começando na Lógica de Programação.
    Façam!

---
+++ contador.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+	int main(){
+	
+	  int  contador = 1 ;
+
+
+	while (contador <= 100) 
+	{
+		
+	printf("Contador: %d\n", contador);
+	contador = contador + 1;
+		
+	}
+	printf("acabou!\n");
+	
+}
