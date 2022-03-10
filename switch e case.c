a3493de83145e770d534b88376eddb62debf5ee0
Vittor-Emanoel <55211291+Vittor-Emanoel@users.noreply.github.com>
2022-03-10 00:36:51 -0300

    Add files via upload

---
+++ switch e case.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+//
+
+int main(void){
+	
+	
+	int num1;
+
+	
+	printf ("Digite um valor de 1 a 7: ");
+ 	 scanf("%d", &num1);
+
+
+	switch(num1){//escolha
+	
+
+		case 1://caso
+		printf("Segunda\n");
+		break; 
+	
+		case 2:
+		printf("Segunda\n");
+		break;
+	
+		case 3:
+		printf("Segunda\n");
+		break;
+	
+		case 4:
+		printf("Segunda\n");
+		break;
+	
+		case 5:
+		printf("Segunda\n");
+		break;
+	
+		case 6:
+		printf("Segunda\n");
+		break;
+	
+		case 7:
+		printf("Segunda\n");
+		break;
+
+		default: 
+		printf("invalido!\n");
+	}
+	
+}
+	
+		
+		
+	
